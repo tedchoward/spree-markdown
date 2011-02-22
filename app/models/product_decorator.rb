b045998c26e5f6d70907530bf8adaa88ed41a70b
Ted Howard <ted@tedchoward.com>
2011-02-21 21:19:26 -0600

    Removed unnecessary logging

--- app/models/product_decorator.rb
+++ app/models/product_decorator.rb
@@ -2,7 +2,6 @@ require 'rdiscount'
 Product.class_eval do
   def description_html
     desc = description.empty? ? 'This product has no description' : description
-    logger.debug description
     RDiscount.new(desc, :smart, :filter_html).to_html
   end
 end
