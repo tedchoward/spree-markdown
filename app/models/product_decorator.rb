bd23748eb0415be0e02b937f5e95ad454b3f6562
Ted Howard <ted@tedchoward.com>
2011-04-03 15:18:39 -0500

    allow html to pass through

--- app/models/product_decorator.rb
+++ app/models/product_decorator.rb
@@ -2,6 +2,6 @@ require 'rdiscount'
 Product.class_eval do
   def description_html
     desc = description.empty? ? 'This product has no description' : description
-    RDiscount.new(desc, :smart, :filter_html).to_html
+    RDiscount.new(desc, :smart).to_html
   end
 end
